f868d6e01051956beb96aac2a364c6ca92f46e7d
Ömer Faruk YILDIRIM <faruk.yildirim@std.izu.edu.tr>
2020-09-03 14:58:14 +0300

    Fixed comment typo

--- SikayetVarComTask/src/main/Quesiton2.java
+++ SikayetVarComTask/src/main/Quesiton2.java
@@ -4,11 +4,11 @@ public class Quesiton2 {
 
 	private static Quesiton2 question2;// private singleton nesnemiz
 
-	private Quesiton2() { // Class'ýmýzý singleton ile oluþturduk
+	private Quesiton2() { // Class Ä±mÄ±zÄ± singleton ile olusturduk
 
 	}
 
-	public static Quesiton2 getInstance() {// Class'ýmýzýn nesne aldýðýmýz methodu
+	public static Quesiton2 getInstance() {// Class'Ä±zÄ±n nesne aldigimiz methodu
 		if (question2 == null) {
 			return question2 = new Quesiton2();
 		}
@@ -17,34 +17,32 @@ public class Quesiton2 {
 	}
 
 	public boolean isUniq(String str) {
-		System.out.println("2.Soru array vs. kullanýlmadan çözüm üretildi.");
+		System.out.println("2.Soru array vs. kullanÃ½lmadan Ã§Ã¶zÃ¼m Ã¼retildi.");
 		try {
-			if (str.matches(".*\\d+.*")) { // regex ile sayý tespit ediyoruz
-				throw new RuntimeException("Girilen string numara veya rakam içeriyor!");
+			if (str.matches(".*\\d+.*")) { // regex ile sayi tespit ediyoruz
+				throw new RuntimeException("Girilen string numara veya rakam iceriyor!");
 			}
-			if (str.matches(".*\\p{Punct}.*")) {// regex ile noktlama iþaretleri tespiti
-				throw new RuntimeException("Girilen string noktalama iþaretleri içeriyor!");
+			if (str.matches(".*\\p{Punct}.*")) {// regex ile noktlama isaretleri tespiti
+				throw new RuntimeException("Girilen string noktalama isaretleri iceriyor!");
 			}
 			if (str.trim().indexOf(" ") != -1) {// trim haricinde whitespace tespit ediyoruz
-				throw new RuntimeException("Girilen string boþluk karakterleri içeriyor!");
+				throw new RuntimeException("Girilen string bosluk karakterleri iceriyor!");
 			}
-			str = str.trim().toLowerCase(); // trim ve lowercase yaptýktan sonra for döngüsüne alýyoruz
-			if (str != null && !str.isEmpty()) { // null ya da empty olum olmamaýsný kontrol ediyoruz
+			str = str.trim().toLowerCase(); // trim ve lowercase yaptiktan sonra for dongusune aliyoruz
+			if (str != null && !str.isEmpty()) { // null ya da empty olum olmamasini kontrol ediyoruz
 				for (int i = 0; i < str.length(); i++) {
 					for (int j = i + 1; j < str.length(); j++) {
-						if (str.charAt(i) == str.charAt(j)) { // her bir harfi kendisinden sonraki kalan harfler ile
-																// karþýlaþtýrýyor ve eðer ayný harf e denk gelirse uniq
-																// olmadýðýna karar verip false dönüyoruz
+						if (str.charAt(i) == str.charAt(j)) { // her bir harfi kendisinden sonraki kalan harfler ile karsilastiriyor ve eger ayni harf e denk gelirse uniq olmadigina karar verip false donuyoruz
 							return false;
 						}
 					}
 				}
 			} else {
-				throw new RuntimeException("Girilen string null yada boþ olamaz!");
+				throw new RuntimeException("Girilen string null yada bos olamaz!");
 			}
 
 		} catch (Exception e) {
-			throw new RuntimeException("Bir hata ile karþýlaþtým! > " + e);
+			throw new RuntimeException("Bir hata ile karsilastim! > " + e);
 		}
 		return true;
 	}
